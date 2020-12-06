82e729923e621c98468dbd09bfd4dc096dcdc306
JesusMachy <65319410+JesusMachy@users.noreply.github.com>
2020-12-06 02:42:43 +0100

    Player Scripts
    Basic player scripts for skills and stats

---
+++ Galaxy Freelancer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    SkillSheet skillSheet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillSheet = GetComponent<SkillSheet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public SkillSheet GetSkillSheet()
+    {
+        return this.skillSheet;
+    }
+}
---
+++ Galaxy Freelancer/Assets/Scripts/SkillSheet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSheet : MonoBehaviour
+{
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Perceptiom { get; set; }
+    public int Inteligence { get; set; }
+    public int Charisma { get; set; }
+
+
+}
